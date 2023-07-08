a922caa4e35f0799f91a0339a6847a13f837846a
Kiran <kiranpuri532@gmail.com>
2023-07-08 12:16:30 +0530

    [Kiran]Add \: insert data in linked list

---
+++ LinkedList/LinkedList/LinkedListDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListDemo
+    {
+        public Node head;
+        public void Insert(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is empty");
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                    temp.next = node;
+
+                }
+                //temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted to linked list",data);
+        }
+
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1-insert data in linked list");
+                Console.WriteLine("choose any above option");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("######## insert data #######");
+                        LinkedListDemo linkesList = new LinkedListDemo();
+                        linkesList.Insert(40);
+                        linkesList.Insert(12);
+                        linkesList.Insert(23);
+                        linkesList.Insert(66);
+                        linkesList.Insert(94);
+                        break;
+                    default:
+                        Console.WriteLine("Invalide option");
+                        break;
+
+                }
+            }
+        }
+    }
+}
