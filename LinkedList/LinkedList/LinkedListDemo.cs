c5f61e44fc773fc8929f3d780224375e063b6350
Kiran <kiranpuri532@gmail.com>
2023-07-08 12:30:15 +0530

    [Kiran]Add \: insert data in linked list

--- LinkedList/LinkedList/LinkedListDemo.cs
+++ LinkedList/LinkedList/LinkedListDemo.cs
@@ -15,21 +15,20 @@ namespace LinkedList
             Node node = new Node(data);
             if (head == null)
             {
-                Console.WriteLine("Linked List is empty");
-            }
+                head = node;            }
             else
             {
                 Node temp = head;
                 while(temp.next != null)
                 {
                     temp = temp.next;
-                    temp.next = node;
 
                 }
-                //temp.next = node;
+                temp.next = node;
             }
             Console.WriteLine("{0} is inserted to linked list",data);
         }
+       
 
     }
 }
